7d76a5331c19795544167df06872f83536242195
HimarRH <himar.rodriguez@gmail.com>
2025-02-23 18:17:25 +0100

    user -> creado componente de tarjetas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { UserHomeComponent } from './views/users/user-home/user-home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: '', redirectTo: '/users', pathMatch: 'full'},
+    {path: 'users', component: UserHomeComponent},
+    //{path: 'users/:id', component: UserDetailComponent},
+
+];
--- src/app/views/users/user-home/user-home.component.ts
+++ src/app/views/users/user-home/user-home.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../../shared/interfaces/user.model';
+import { UserService } from '../../../shared/services/user.service';
+import { CardComponent } from "../../../shared/components/card/card.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-home',
-  imports: [],
+  imports: [CardComponent, CommonModule],
   templateUrl: './user-home.component.html',
   styleUrl: './user-home.component.css'
 })
-export class UserHomeComponent {
+export class UserHomeComponent implements OnInit {
+
+  users: User[] =[]
+
+  constructor (private userService: UserService) {
+
+  }
+
+  ngOnInit(): void {
+    this.getUsers();
+
+  }
+
+  public onViewDetail(id: number){
+    console.log('view detail ' + id);
+  }
+
+  public onEdit(id: number){
+    console.log('edit' + id);
+  }
+
+  public onDelete(id: number){
+    console.log('delete' + id);
+  }
+
+  private getUsers(){
+    this.userService.getUsers().subscribe(
+      (users) => {
+        this.users = users.results;
+        console.log(this.users);
+      }
+    );
+  }
 
 }
